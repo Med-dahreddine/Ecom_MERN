2e83b39c5f5b5007a9b92e73db69bf21a0d83b60
Med-dahreddine <dahreddine.med2@gmail.com>
2022-06-21 09:39:29 +0100

    adding schema of user

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+//Import Routes
+const userRoutes = require("./routes/users");
+
+//config App
 const app = express();
+require("dotenv").config();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+console.log(process.env.DATABASE);
 
-app.listen(3000, () => console.log("the app listening the port 3000"));
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => console.log("db connectd"))
+  .catch(() => console.error("not connect to the database"));
+
+//Routes Middleware
+app.use("/api/users", userRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`app is running on port ${port} ...`));
